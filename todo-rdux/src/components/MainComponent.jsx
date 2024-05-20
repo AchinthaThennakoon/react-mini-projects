679062b33446a06da78c992197e66ade55782ef3
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-05-20 17:46:21 +0530

    add dispatch function to update to store

--- todo-rdux/src/components/MainComponent.jsx
+++ todo-rdux/src/components/MainComponent.jsx
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDoAction, removeToDoAction } from "../actions/ToDoActions";
 
 const MainComponent = () => {
+  const [todo, setTodo] = useState();
+  const dispatch = useDispatch();
+  const Todo = useSelector((state) => state.Todo);
+  const { todos } = Todo;
+
+  const handleSubmit = (e) => {
+    if (todo === "") {
+      return;
+    }
+
+    e.preventDefault();
+    dispatch(addToDoAction(todo));
+    setTodo("");
+  };
+
+  const handleDelete=(todo)=>{
+    dispatch(removeToDoAction(todo));
+  }
+
   return (
     <>
       <div class="container">
         <h1>ToDo List</h1>
 
-        <form class="input-container" onsubmit="return false">
+        <form class="input-container" onSubmit={handleSubmit}>
           <input
             type="text"
             name="item"
             id="newToDo"
             placeholder="Enter your todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
           />
           <input type="submit" id="addBtn" />
         </form>
 
         <ul id="todolist">
-          <li>
-            <span>todo 1</span>
-            <button className="delete-todo-btn">X</button>
-          </li>
+          {todos &&
+            todos.map((todo) => (
+              <li id={todo.id}>
+                <span>{todo.todo}</span>
+                <button className="delete-todo-btn" onClick={()=>handleDelete(todo.id)}>X</button>
+              </li>
+            ))}
         </ul>
       </div>
     </>
   );
-}
+};
 
-export default MainComponent
+export default MainComponent;
